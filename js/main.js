ce46810f65f72bbf5b4fdbecc4b46b543c8a1391
Andres <patux@patux.cl>
2015-06-25 13:40:40 -0300

    Ajustes owlCarousel

--- js/main.js
+++ js/main.js
@@ -6,69 +6,69 @@
 
 function main() {
 
-(function () {
-   'use strict';
+	(function () {
+		'use strict';
 
-   /* ==============================================
+		/* ==============================================
   	Testimonial Slider
   	=============================================== */
 
-  	$('a.page-scroll').click(function() {
-        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-          var target = $(this.hash);
-          target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-          if (target.length) {
-            $('html,body').animate({
-              scrollTop: target.offset().top - 40
-            }, 900);
-            return false;
-          }
-        }
-      });
+		$('a.page-scroll').click(function () {
+			if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+				var target = $(this.hash);
+				target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+				if (target.length) {
+					$('html,body').animate({
+						scrollTop: target.offset().top - 40
+					}, 900);
+					return false;
+				}
+			}
+		});
 
-    /*====================================
-    Show Menu on Book
-    ======================================*/
-    $(window).bind('scroll', function() {
-        var navHeight = $(window).height() - 100;
-        if ($(window).scrollTop() > navHeight) {
-            $('.navbar-default').addClass('on');
-        } else {
-            $('.navbar-default').removeClass('on');
-        }
-    });
+		/*====================================
+		Show Menu on Book
+		======================================*/
+		$(window).bind('scroll', function () {
+			var navHeight = $(window).height() - 100;
+			if ($(window).scrollTop() > navHeight) {
+				$('.navbar-default').addClass('on');
+			} else {
+				$('.navbar-default').removeClass('on');
+			}
+		});
 
-    $('body').scrollspy({
-        target: '.navbar-default',
-        offset: 80
-    })
+		$('body').scrollspy({
+			target: '.navbar-default',
+			offset: 80
+		})
 
-  	$(document).ready(function() {
-  	  $("#team").owlCarousel({
-
-  	      navigation : false, // Show next and prev buttons
-  	      slideSpeed : 300,
-  	      paginationSpeed : 400,
-  	      autoHeight : true,
-  	      itemsCustom : [
+		$(document).ready(function () {
+			$("#team").owlCarousel({
+				items: 6,
+				navigation: true, // Show next and prev buttons
+				slideSpeed: 300,
+				paginationSpeed: 400,
+				autoHeight: true,
+				itemsCustom: [
 				        [0, 1],
-				        [450, 2],
-				        [600, 2],
-				        [700, 2],
-				        [1000, 4],
-				        [1200, 4],
-				        [1400, 4],
-				        [1600, 4]
+				        [450, 6],
+				        [600, 6],
+				        [700, 6],
+				        [1000, 6],
+				        [1200, 6],
+				        [1400, 6],
+				        [1600, 6]
 				      ],
-  	  });
+			});
 
-  	  $("#clients").owlCarousel({
+			$("#clients").owlCarousel({
 
-  	      navigation : false, // Show next and prev buttons
-  	      slideSpeed : 300,
-  	      paginationSpeed : 400,
-  	      autoHeight : true,
-  	      itemsCustom : [
+				navigation: false, // Show next and prev buttons
+				slideSpeed: 300,
+				paginationSpeed: 400,
+				autoHeight: true,
+				itemsCustom: [
 				        [0, 1],
 				        [450, 2],
 				        [600, 2],
@@ -78,50 +78,50 @@ function main() {
 				        [1400, 5],
 				        [1600, 5]
 				      ],
-  	  });
+			});
 
-      $("#testimonial").owlCarousel({
-        navigation : false, // Show next and prev buttons
-        slideSpeed : 300,
-        paginationSpeed : 400,
-        singleItem:true
-        });
+			$("#testimonial").owlCarousel({
+				navigation: false, // Show next and prev buttons
+				slideSpeed: 300,
+				paginationSpeed: 400,
+				singleItem: true
+			});
 
-  	});
+		});
 
-  	/*====================================
+		/*====================================
     Portfolio Isotope Filter
     ======================================*/
-    $(window).load(function() {
-        var $container = $('#lightbox');
-        $container.isotope({
-            filter: '*',
-            animationOptions: {
-                duration: 750,
-                easing: 'linear',
-                queue: false
-            }
-        });
-        $('.cat a').click(function() {
-            $('.cat .active').removeClass('active');
-            $(this).addClass('active');
-            var selector = $(this).attr('data-filter');
-            $container.isotope({
-                filter: selector,
-                animationOptions: {
-                    duration: 750,
-                    easing: 'linear',
-                    queue: false
-                }
-            });
-            return false;
-        });
+		$(window).load(function () {
+			var $container = $('#lightbox');
+			$container.isotope({
+				filter: '*',
+				animationOptions: {
+					duration: 750,
+					easing: 'linear',
+					queue: false
+				}
+			});
+			$('.cat a').click(function () {
+				$('.cat .active').removeClass('active');
+				$(this).addClass('active');
+				var selector = $(this).attr('data-filter');
+				$container.isotope({
+					filter: selector,
+					animationOptions: {
+						duration: 750,
+						easing: 'linear',
+						queue: false
+					}
+				});
+				return false;
+			});
 
-    });
+		});
 
 
 
-}());
+	}());
 
 
 }
